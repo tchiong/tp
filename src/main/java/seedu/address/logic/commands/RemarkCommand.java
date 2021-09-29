b32119d2197f216080563cd36121ad5d0030aeeb
tchiong <57283702+tchiong@users.noreply.github.com>
2021-09-29 17:54:02 +0800

    Bug Fixes

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -53,3 +53,4 @@ public class RemarkCommand extends Command {
             return false;
         }
     }
+}
--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -116,9 +116,9 @@ public class Person {
                 .append("; Email: ")
                 .append(getEmail())
                 .append("; Address: ")
-                .append(getAddress());
+                .append(getAddress())
                 .append(" Remark: ")
-                .append(getRemark())
+                .append(getRemark());
 
         Set<Tag> tags = getTags();
         if (!tags.isEmpty()) {
