5e81e7bc075efdde9c6ca1c570a5061413d2cd6d
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-10-07 17:08:25 +0800

    Update addAppCommand to create a new Appointment
    A new appointment needs to be made to be added.
    
    Creating the appointment in addAppCommand allows us to use the list
    in the model to obtain the client.
    
    Let's use the index given to obtain the Person object that represents
    the client and along with the other fields, create an Appointment.
    
    What needs to be done:
    * Update the model to use the Schedule class
    * Modify the execute method to properly add the appointment to the
    Schedule in model.

--- src/main/java/seedu/address/logic/commands/AddAppCommand.java
+++ src/main/java/seedu/address/logic/commands/AddAppCommand.java
@@ -1,7 +1,23 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Person;
+import seedu.address.model.schedule.Appointment;
 
 /**
  * Creates an appointment with an existing person in PlaceBook
@@ -10,8 +26,81 @@ public class AddAppCommand extends Command {
 
     public static final String COMMAND_WORD = "addApp";
 
+    public static final String NOT_IMPLEMENTED_MESSAGE = "Command not implemented yet";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds an appointment to PlaceBook. "
+            + "Parameters: "
+            + "INDEX "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + PREFIX_DATE + "DATE (dd-MM-yyyy) "
+            + PREFIX_TIME + "TIME (hhmm)"
+            + PREFIX_DESCRIPTION + "DESCRIPTION "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_ADDRESS + "Starbucks @ Raffles City "
+            + PREFIX_DATE + "14-12-2021"
+            + PREFIX_TIME + "1400"
+            + PREFIX_DESCRIPTION + "discuss marketing strategies";
+
+    public static final String MESSAGE_SUCCESS = "New appointment added: %1$s";
+
+    private final Index index;
+    private final Address location;
+    private final LocalDate date;
+    private final LocalTime time;
+    private final String description;
+
+    public AddAppCommand(Index index, Address location, LocalDate date, LocalTime time, String description) {
+        requireNonNull(index);
+        requireNonNull(location);
+        requireNonNull(date);
+        requireNonNull(time);
+        requireNonNull(description);
+
+        this.index = index;
+        this.location = location;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("Hello from addApp");
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person client = lastShownList.get(index.getZeroBased());
+        Appointment newAppointment = new Appointment(client, location, date, description, time);
+
+        // TODO
+        // model.addAppointment(newAppointment)
+        // return new CommandResult(String.format(MESSAGE_SUCCESS, newAppointment));
+
+        // Can remove once schedule is implemented
+        throw new CommandException(NOT_IMPLEMENTED_MESSAGE);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddAppCommand)) {
+            return false;
+        }
+
+        AddAppCommand aa = (AddAppCommand) other;
+        return this.index.equals(aa.index)
+                && this.location.equals(aa.location)
+                && this.date.equals(aa.date)
+                && this.time.equals(aa.time)
+                && this.description.equals(aa.description);
     }
 }
