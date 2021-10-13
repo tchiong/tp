73fc2b4088bbf7ef8e0cf522c2e6c05466265a40
YaleChen299 <proverb299@gmail.com>
2021-10-13 21:43:31 +0800

    fix javadoc

--- src/main/java/seedu/address/logic/parser/FindAppCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindAppCommandParser.java
@@ -16,7 +16,7 @@ public class FindAppCommandParser implements Parser<FindAppCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FindAppCommand
      * and returns a FindAppCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform to the expected format
      */
     public FindAppCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
