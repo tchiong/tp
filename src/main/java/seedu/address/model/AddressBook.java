ed5b1525385978e3f1812249f399f68581262174
Yew Meng <yewmengchai@gmail.com>
2021-10-13 16:52:31 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -110,7 +110,9 @@ public class AddressBook implements ReadOnlyAddressBook {
     /**
      * Removes Appointment from the address book
      */
-    public void deleteAppointment(Appointment a) { appointments.remove(a); }
+    public void deleteAppointment(Appointment a) {
+        appointments.remove(a);
+    }
 
     public Appointment getAppointment(int index) {
         return appointments.get(index);
