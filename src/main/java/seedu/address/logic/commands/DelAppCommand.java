346da63f77b4893818a69b5b5f23fd59c595ac2e
Yew Meng <yewmengchai@gmail.com>
2021-10-13 16:46:28 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/logic/commands/DelAppCommand.java
+++ src/main/java/seedu/address/logic/commands/DelAppCommand.java
@@ -1,14 +1,15 @@
 package seedu.address.logic.commands;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.schedule.Appointment;
 
-import java.util.List;
-
 import static java.util.Objects.requireNonNull;
+
 
 /**
  * Deletes an existing appointment in the schedule
