d375bd55508e00cde858521f1b3e9e7839c1f510
tchiong <57283702+tchiong@users.noreply.github.com>
2021-10-14 17:16:14 +0800

    Fix Check Style

--- src/test/java/seedu/address/storage/JsonSerializableScheduleTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableScheduleTest.java
@@ -1,22 +1,24 @@
 package seedu.address.storage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.schedule.Schedule;
 import seedu.address.testutil.TypicalAppointment;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.testutil.Assert.assertThrows;
-
 public class JsonSerializableScheduleTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableScheduleTest");
     private static final Path TYPICAL_APPOINTMENT_FILE = TEST_DATA_FOLDER.resolve("typicalAppointmentSchedule.json");
-    private static final Path DUPLICATE_APPOINTMENT_FILE = TEST_DATA_FOLDER.resolve("duplicateAppointmentSchedule.json");
+    private static final Path DUPLICATE_APPOINTMENT_FILE =
+            TEST_DATA_FOLDER.resolve("duplicateAppointmentSchedule.json");
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
