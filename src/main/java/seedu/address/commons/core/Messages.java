23ada63ab43546e064358c7fb3d7c1f3368643f0
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-10-23 14:29:48 +0800

    Add error message when user adds duplicate Person

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -12,5 +12,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_APPOINTMENT_DISPLAYED_INDEX =
             "The appointment index provided is invalid";
     public static final String MESSAGE_APPOINTMENTS_LISTED_OVERVIEW = "%1$d appointments listed!";
+    public static final String MESSAGE_APPOINTMENTS_DUPLICATE_PERSON_ADDED = "Duplicated Person not allowed!";
 
 }
